c663fed5520d37ed09a75754ab80bbe9edce7296
Lupitaglz <lupitaglzl84@gmail.com>
2022-07-19 23:36:13 -0500

    feat: added a component called grandchild

--- src/componentes/ComponenteNieto/ComponenteNieto.jsx
+++ src/componentes/ComponenteNieto/ComponenteNieto.jsx
@@ -3,7 +3,6 @@ import ComponenteBisNieto from "../CompontenteBisNieto/ComponenteBisNieto";
 export default function ComponenteNieto() {
     return(
         <div>
-            
             <ComponenteBisNieto />
         </div>
     )
