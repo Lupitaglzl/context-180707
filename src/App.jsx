cd42f2f5f387474050a09e434cf4b91bbfc0e88a
Lupitaglz <lupitaglzl84@gmail.com>
2022-07-19 23:25:45 -0500

    feat: work done in practical class

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import { useContext } from 'react';
+import './App.css';
+import ComponenteHijo from "./componentes/ComponenteHijo";
+import {GlobalContext} from "./context/global/global.context";
+//import {useTraslation} from "react-i18next";
+import mifoto from '../src/assets/mifoto.jpeg';
+import {FaDropbox, FaStripe, FaGithub,} from 'react-icons/fa'; 
+
+
+function App() {
+  const { add, substract} = useContext (GlobalContext);
+  //const {i18n, t} = useTraslation();
+  //const [language,setLenguage] =useState('es'); 
+
+  /*function changeLanguage(language){
+    i18n.changeLanguage(language);
+  }*/
+
+  return (
+    <div className="App">
+      <div className='menu'>
+        <nav className='nav'>
+           <ul>
+            <li><a className='me' href='#'>Home</a></li>
+            <li><a className='me' href='#'>Español</a></li>
+            <li><a className='me' href='#'>Ingles</a></li>
+          </ul>
+        </nav>
+      </div>
+      <div className='img'>
+          <img src={mifoto}
+        />
+        </div>
+        <br/>
+      <header className="App-header">
+        <h1 className='titulo'>Guadalupe del Rocio Gonzalez Limon</h1>
+        <br/>
+        <p className='parrafo'>A mi me gusta escuchar musica, leer, ver peliculas y series, cuando termine la carrera,
+         espero poder trabajar como desarrolladora y seguir aprendiendo mas cosas sobre la carrera</p>
+        <br/>
+        <button  className=' btn'
+          style={{
+            padding:'2%',
+            width: "13vn", 
+            backgroundColor:"rgb(64, 255, 47)", 
+            cursor: "pointer",
+            margin:"1vn"
+            
+          }} 
+          onClick={add}>Contar vistas > </button>
+        
+        <ComponenteHijo />
+
+            <a className='icon' href='#'><FaDropbox/></a>
+            <a className='icon' href='#'><FaStripe/></a>
+            <a className='icon' href='#'><FaGithub/></a>
+            
+      </header>
+      
+    </div>
+  );
+
+}
+
+
+
+export default App;
